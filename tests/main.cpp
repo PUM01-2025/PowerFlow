78150cd6d73aa5cb0a6ae007383b5090414ea955
simonhansson <simha158@student.liu>
2025-04-09 15:07:01 +0200

    Added Catch2 and created a simple input validation test

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,14 +1,61 @@
-//#include <add.h>
+#define CATCH_CONFIG_MAIN
 
-#include <cassert>
-#include <vector>
-#include "powerflow/network.hpp"
+#include "catch.hpp"
+#include "powerflow/NetworkLoader.hpp"
 
-int main() {
-    std::vector<int> v;
-    Network n;
-    //assert(add(5, 5) == 10);
-   // assert(add(1, 1) == 2);
-    //assert(add(0, -1) == -1);
-    return 0;
+#include <fstream>
+#include <string>
+
+int add(int a,int b){
+    return a+b;
+}
+
+
+// TEST_CASE("DEBUG", "[debug]"){
+//     REQUIRE(add(5,5) == 1);
+// }
+
+bool hasEnding (std::string const &fullString, std::string const &ending) {
+    if (fullString.length() >= ending.length()) {
+        return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
+    } else {
+        return false;
+    }
+}
+
+
+//CHECK_FALSE(file.fail());  checks that the file can be opened correctly (in most cases it is the wrong filepath)
+TEST_CASE("Networkloader input", "[!throws]" ) {
+    
+    SECTION("Invalid command"){
+        std::ifstream file("/Users/simonhansson/U3/Kandidat01/examples/invalid_command_network.txt");
+        CHECK_FALSE(file.fail()); 
+        NetworkLoader loader(file);
+        REQUIRE_THROWS_AS(loader.loadNetwork() , std::runtime_error);
+        REQUIRE_THROWS_WITH(loader.loadNetwork(), Catch::Matchers::Contains("Invalid command"));
+    }
+
+    /*
+    #These tests are deprecated as the cover cases that can't be reached
+
+    SECTION("Invalid node count"){
+        std::ifstream file("/Users/simonhansson/U3/Kandidat01/examples/invalid_node_count_network.txt");
+        CHECK_FALSE(file.fail()); 
+        NetworkLoader loader(file);
+        loader.loadNetwork();
+
+        REQUIRE_THROWS_AS(loader.loadNetwork() , std::runtime_error);
+        REQUIRE_THROWS_WITH(loader.loadNetwork(), Catch::Matchers::Contains("Invalid node count"));
+    }
+
+    SECTION("Missing end of list (%)"){
+        std::ifstream file("/Users/simonhansson/U3/Kandidat01/examples/missing_end_list_network.txt");
+        CHECK_FALSE(file.fail()); 
+        NetworkLoader loader(file);
+        loader.loadNetwork();
+
+        REQUIRE_THROWS_AS(loader.loadNetwork() , std::runtime_error);
+        REQUIRE_THROWS_WITH(loader.loadNetwork(), Catch::Matchers::Contains("Missing end-of-list indicator"));
+    }
+    */
 }
