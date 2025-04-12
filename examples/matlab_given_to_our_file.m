5e3e1a67b555f5c0b2d910722230a15c37633066
Malte Hammar <malte.hammar01@gmail.com>
2025-04-12 17:08:30 +0200

    Add start of matlab to our file

---
+++ examples/matlab_given_to_our_file.m
@@ -0,0 +1,26 @@
+fprintf('Loading grid data... ');
+grid_directory = '+data/';
+%grid_file      = 'S433_Ekangen_Validation';
+grid_file      = 'S495_Linghem_Validation';
+
+
+
+% Load grid data
+grid_data = load([grid_directory, grid_file]);
+
+
+% Now we have the whole structure and can generate the network:
+
+%put out "grid"
+
+% for pair in grid_data.connectionBuses
+    % put out the pair and get the Z_ser[i,j] ( or Y_shy[i.j] ? )
+ 
+% put %
+
+% get all iindex where gird_data.busisLoad == 1 
+    % put them as load
+% put %
+
+% put connections
+% put %
