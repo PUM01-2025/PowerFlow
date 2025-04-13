b63986f9976b86698d6a04d99c8bcfec58799835
Malte Hammar <malte.hammar01@gmail.com>
2025-04-13 10:42:47 +0200

    Nu puttar den ut nätverk tror jag

--- examples/matlab_given_to_our_file.m
+++ examples/matlab_given_to_our_file.m
@@ -1,5 +1,8 @@
-fprintf('Loading grid data... ');
-grid_directory = '+data/';
+clear
+
+fprintf('Loading grid data... \n');
+%grid_directory = '+data/';
+grid_directory = '';
 %grid_file      = 'S433_Ekangen_Validation';
 grid_file      = 'S495_Linghem_Validation';
 
@@ -8,19 +11,53 @@ grid_file      = 'S495_Linghem_Validation';
 % Load grid data
 grid_data = load([grid_directory, grid_file]);
 
+%open file
+
+fd = fopen("output.txt", "w");
+
 
 % Now we have the whole structure and can generate the network:
 
 %put out "grid"
 
+fprintf(fd, "grid\n");
+
 % for pair in grid_data.connectionBuses
     % put out the pair and get the Z_ser[i,j] ( or Y_shy[i.j] ? )
- 
+
+connections = grid_data.connectionBuses;
+
+for k = 1:size(connections,1)
+    i = connections(k,1);
+    j = connections(k,2);
+    z = grid_data.Z_ser(i,j);
+    % We have to do - 1 since matlab is 1 indexed :(
+    fprintf(fd,"%d %d (%f, %f)\n", i - 1, j - 1, real(z), imag(z));
+end
+% put %
+fprintf(fd, "%%\n");
+
+% get all index where gird_data.busisLoad == 1 
+    % put them as load
+is_load = grid_data.busIsLoad;
+
+for k = 1:size(is_load,1)
+    if is_load(k)
+        % We have to do - 1 since matlab is 1 indexed :(
+        fprintf(fd,"%d\n", k-1);
+    end
+end
+
+    
 % put %
 
-% get all iindex where gird_data.busisLoad == 1 
-    % put them as load
-% put %
+fprintf(fd, "%%\n");
 
 % put connections
+fprintf(fd, "connections\n");
+
 % put %
+fprintf(fd, "%%");
+fprintf("Data converted! \n");
+
+fclose(fd);
