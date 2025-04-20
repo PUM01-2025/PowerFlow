11c81a79bbcc2fe9e3f01bebe049b415e31b642f
samma466 <samma466@student.liu.se>
2025-04-20 15:12:10 +0200

    Fixat pybind

---
+++ examples/python_interface.py
@@ -0,0 +1,5 @@
+import python_wrappers
+
+print("Resultat av 5+5 är: ")
+print(python_wrappers.pybind11_add(5, 5))
+
--- python/python_wrappers.cpp
+++ python/python_wrappers.cpp
@@ -1,9 +1,9 @@
 #include <pybind11/pybind11.h>
 #include "python_wrappers.hpp"
 #include <unordered_map>
-#include "powerflow/network.hpp"
-#include "powerflow/PowerFlowSolver.hpp"
-#include "powerflow/NetworkLoader.hpp"
+// #include "powerflow/network.hpp"
+// #include "powerflow/PowerFlowSolver.hpp"
+// #include "powerflow/NetworkLoader.hpp"
 #include <fstream>
 #include <memory>
 
@@ -14,7 +14,7 @@ float pybind11_add(float a, float b)
 
 PYBIND11_MODULE(python_wrappers, m)
 {
-    m.doc() = "Docstring for pybind11 module =============";
+    m.doc() = "Docstring for pybind11 module ============= ";
     m.def("pybind11_add", &pybind11_add, "Addition funtion",
-          pypind11::arg("a"), pybind11::arg("b"));
+          pybind11::arg("a"), pybind11::arg("b"));
 }
