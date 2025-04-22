022c918f7e817188604803296b08b61a9d275a5b
samma466 <samma466@student.liu.se>
2025-04-22 13:42:25 +0200

    Halvt fungerande

--- examples/python_interface.py
+++ examples/python_interface.py
@@ -2,3 +2,12 @@ import python_wrappers
 
 file_path = "../examples/example_network_single_grid.txt"
 
+solver = python_wrappers.PowerFlowWrapper(file_path)
+
+P = [1.0 + 0.5j, 0.7 + 0.2j, 0.9 + 0.4j]
+
+U = solver.solve(P)
+
+
+for i in U: 
+    print(i)
--- python/python_wrappers.cpp
+++ python/python_wrappers.cpp
@@ -1,5 +1,6 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <pybind11/complex.h>
 
 #include <memory>
 #include <complex>
