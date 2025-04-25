23e9f21e13d646ea26d4148dca7521654a413c24
Oliver Brenner <oliver.bif2001@gmail.com>
2025-04-25 14:14:22 +0200

    NetworkAnalyzer returns error if the graphs are disjoint

--- src/NetworkAnalyzer.cpp
+++ src/NetworkAnalyzer.cpp
@@ -22,6 +22,63 @@
 // 3.If an adjacent vertex is already visited and is not the parent of the current vertex, a cycle is detected, and True is returned.
 // 4. If DFS traversal is done on all vertices and no cycles are detected, False is returned.
 
+
+    
+bool has_multiple_graphs(Grid const& grid)
+{
+    // Returns true if network has mulitple disjoint graphs
+    std::stack<node_idx_t> todo_list{};
+    std::unordered_set<node_idx_t> visited_nodes{};
+    std::unordered_set<edge_idx_t> visited_edges{};
+
+    // Push root node
+    todo_list.push(0);
+
+    while (!todo_list.empty())
+    {
+        // Get current node
+        node_idx_t current_node = todo_list.top();
+        todo_list.pop();
+        // Insert as visited,
+        visited_nodes.insert(current_node);
+
+        // add neighbors to stack
+        GridNode current_grid_node = grid.nodes[current_node];
+
+        // for all edges of current node, add neighbours to stack
+        for (edge_idx_t id : current_grid_node.edges)
+        {
+
+            // If edge exists in visited, we shall not go there again
+            if (visited_edges.find(id) != visited_edges.end())
+            {
+                continue;
+            }
+
+            node_idx_t parent{ grid.edges[id].parent };
+            node_idx_t child{ grid.edges[id].child };
+
+            // If node is child --> push parent to stack
+            if (parent != current_node)
+            {
+                todo_list.push(parent);
+            } // If node is parent --> push child to stack
+            else if (child != current_node)
+            {
+                todo_list.push(child);
+            }
+            else
+            {
+                std::cerr << "Error! found edge with myself not in it!" << std::endl;
+            }
+
+            visited_edges.insert(id);
+        }
+    }
+
+    return !(visited_nodes.size() == grid.nodes.size()); 
+}
+
 bool has_cycles(Grid const& grid)
 {
     // Returns true if the Grid contains a cycle
@@ -85,6 +142,11 @@ bool has_cycles(Grid const& grid)
 SolverType determine_solver(Grid const& grid)
 {
     // Returns Solvertype Enum depending on grid structure
+
+    if(has_multiple_graphs(grid)){
+        std::cerr << "Error! The network consists of several graphs. Make sure they are joint by adding edge" << std::endl;
+    }
+
     if (has_cycles(grid))
     {
         return GAUSSSEIDEL;
