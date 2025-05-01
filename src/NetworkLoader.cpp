5faa4d747418e14b14ffcf30ca72865fedf9da13
gussj945 <gussj945@student.liu.se>
2025-05-01 11:53:07 +0200

    fixat kontroll av slacknät och mellannät samt noder

--- src/NetworkLoader.cpp
+++ src/NetworkLoader.cpp
@@ -2,6 +2,8 @@
 #include "powerflow/NetworkLoaderError.hpp"
 #include <set>
 
+/** Loads a network file, checks if the network is acceptable if it is an 
+unique pointer is returned, if not an error is thrown*/
 NetworkLoader::NetworkLoader(std::istream& file) : file{ file } { }
 
 std::unique_ptr<Network> NetworkLoader::loadNetwork()
@@ -17,7 +19,7 @@ std::unique_ptr<Network> NetworkLoader::loadNetwork()
         }
         else if (line == "connections")
         {
-            network->connections = loadConnections();
+            network->connections = loadConnections(*network);
         }
         else
         {
@@ -162,7 +164,7 @@ void NetworkLoader::getGridBase(Grid& grid)
     }
 }
 
-std::vector<GridConnection> NetworkLoader::loadConnections()
+std::vector<GridConnection> NetworkLoader::loadConnections(const Network& network)
 {
     std::vector<GridConnection> connections;
     std::string line;
@@ -192,6 +194,35 @@ std::vector<GridConnection> NetworkLoader::loadConnections()
         {
             throw NetworkLoaderError("Invalid PQ node index", curLine);
         }
+
+        //Check if the middle grid exists
+        if (network.grids.size() < connection.pqGrid) {
+            throw NetworkLoaderError("The middle grid " + std::to_string(connection.pqGrid) +
+                " doesn't exist", curLine);
+        }
+        //Check if the slack node exists
+        if (network.grids.at(connection.pqGrid).nodes.size() < connection.slackNode) {
+            throw NetworkLoaderError("The slack node " + std::to_string(connection.slackNode) +
+                "in the middle grid doesn't exist", curLine);
+        }
+        //Check if the slack grid exists
+        if (network.grids.size() < connection.slackGrid) {
+            throw NetworkLoaderError("The middle grid " + std::to_string(connection.slackGrid) +
+                " doesn't exist", curLine);
+        }
+        //Check if the middle node exists
+        if (network.grids.at(connection.slackGrid).nodes.size() < connection.pqNode) {
+            throw NetworkLoaderError("The slack node " + std::to_string(connection.pqNode) +
+                "in the middle grid doesn't exist", curLine);
+        }
+
+
+       
+
+
+
+
+
         connections.push_back(connection);
 
         // Clear the stringstream for the next line
