c8778d1bd5b8b0d7c3d2186e7e1d90f9d7496645
Malte Hammar <malte.hammar01@gmail.com>
2025-05-19 16:57:04 +0200

    Add support for top BFS tree in generate test network

--- misc/generate_test_network.py
+++ misc/generate_test_network.py
@@ -5,7 +5,8 @@ import typing
 def generate_grid(file: typing.TextIO, grid_no: int) -> tuple[int, int]:
 
     network = {}
-    grid_size = random.randint(20, 200)
+    grid_size = random.randint(2, 200)
+    #grid_size = 35
     file.write("grid\n")
     file.write(f"# {grid_no} \n")
     file.write("10000000 400\n")
@@ -23,7 +24,7 @@ def generate_grid(file: typing.TextIO, grid_no: int) -> tuple[int, int]:
 
     file.write("%\n")
 
-    file.write("0 s\n")
+    file.write("0 si\n")
 
     load_nodes = 0
 
@@ -40,6 +41,7 @@ def generate_grid(file: typing.TextIO, grid_no: int) -> tuple[int, int]:
 def generate_network(file_name: str, loops: bool = False):
     with open(file_name, "w") as f:
         networks = random.randint(2000, 4000)
+        networks = 3500
         size = 1
         loads = 0
         for i in range(0, networks):
@@ -50,11 +52,19 @@ def generate_network(file_name: str, loops: bool = False):
         f.write("grid\n")
         f.write("1000000000 10000\n")
         for i in range(1, networks+1):
-            f.write(f"{i} {i-1} (0.0005, 0.0005)\n")
-        f.write(f"{0} {i} (0.0005, 0.0005)\n")
+            if loops:
+                f.write(f"{i} {i-1} (0.0005, 0.0005)\n")
+            else:
+                f.write(f"{i} {0} (0.0005, 0.0005)\n")
+        
+        if loops:
+            f.write(f"{0} {i} (0.0005, 0.0005)\n")
 
         f.write("%\n")
-        f.write("0 e\n")
+        f.write("0 s\n")
+        for i in range(1, networks+1):
+            f.write(f"{i} li\n")
+
         f.write("%\n")
         f.write("connections\n")
         for i in range(1, networks+1):
@@ -66,6 +76,6 @@ def generate_network(file_name: str, loops: bool = False):
 
 
 if __name__ == "__main__":
-    generate_network("stort_natverk.txt")
+    generate_network("stort_natverk.txt", False)
 
     # generate_grid(1)
